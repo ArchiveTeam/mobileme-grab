1af550e517edce87b9d231257a80e3f4cdc4e2f7
Alard <alard@example.com>
2011-10-16 23:44:33 +0200

    web.me.com: WebDAV downloading.
    web.me.com: A new script downloads the WebDAV index of the full
    web directory of a user. This gives us all files, even those that
    are not linked to.
    
    homepage.mac.com: The old web.me.com/homepage.mac.com script.

--- dld-homepage-mac-com.sh
+++ dld-homepage-mac-com.sh
@@ -4,14 +4,28 @@
 #
 # Usage:   dld-web-me-com.sh ${USERNAME}
 #
-# Version 3. Started with a new script for web.me.com.
+# To download homepage.mac.com instead of web.me.com:
+#
+# Usage:   dld-web-me-com.sh ${USERNAME} homepage.mac.com
+#
+# Version 2. Added homepage.mac.com.
+# Version 1.
 #
 
+if [[ ! -x $PHANTOMJS ]]
+then
+  PHANTOMJS=$(which phantomjs)
+fi
 if [[ ! -x $WGET_WARC ]]
 then
   WGET_WARC=$(which wget)
 fi
 
+if [[ ! -x $PHANTOMJS ]]
+then
+  echo "phantomjs not found. Set the PHANTOMJS environment variable."
+  exit 3
+fi
 if [[ ! -x $WGET_WARC ]]
 then
   echo "wget not found. Set the WGET_WARC environment variable."
@@ -25,7 +39,14 @@ fi
 
 username="$1"
 
+domain="web.me.com"
 userdir="data/${username:0:1}/${username:0:2}/${username:0:3}/${username}/web"
+
+if [[ $2 == "homepage.mac.com" ]]
+then
+  domain="homepage.mac.com"
+  userdir="data/${username:0:1}/${username:0:2}/${username:0:3}/${username}/homepage"
+fi
 
 if [[ -f "${userdir}/.incomplete" ]]
 then
@@ -44,15 +65,14 @@ touch "${userdir}/.incomplete"
 
 echo "Downloading ${username}"
 
-echo -n " - Discovering urls..."
-$WGET_WARC -q -O "$userdir/webdav-feed.xml" "http://web.me.com/${username}/?webdav-method=truthget&depth=infinity"
-grep -oE "http://web\.me\.com\/[^\"]+" "$userdir/webdav-feed.xml" > "$userdir/urls.txt"
-count=$( cat "$userdir/urls.txt" | wc -l )
-echo " done."
+echo " - Discovering urls (takes a while)"
+$PHANTOMJS discover.coffee "http://${domain}/${username}" > urls-$$.txt
+count=`cat urls-$$.txt | wc -l`
 
-echo -n " - Downloading (${count} files)..."
-$WGET_WARC -nv -o "$userdir/wget.log" -i "$userdir/urls.txt" -O /dev/null --warc-file="$userdir/$username" --warc-max-size=inf
-echo " done."
+echo " - Downloading (${count} files)"
+$WGET_WARC -nv -o "$userdir/wget.log" -i urls-$$.txt -O /dev/null --warc-file="$userdir/$username" --warc-max-size=inf
+
+rm urls-$$.txt
 
 rm "${userdir}/.incomplete"
 
