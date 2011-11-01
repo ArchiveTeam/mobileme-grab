4b8ef81f1d723c4eec981f3cc8e0934f29c3b260
Alard <alard@example.com>
2011-11-01 10:11:36 +0100

    Updated wget version.

--- get-wget-warc.sh
+++ get-wget-warc.sh
@@ -3,10 +3,10 @@
 # This script downloads and compiles wget-warc.
 #
 
-rm -rf wget-warc-20111017.tar.bz2 wget-1.13.4-2567-dirty/
+rm -rf wget-warc-20111101.tar.bz2 wget-1.13.4-2567-dirty/
 
-wget https://github.com/downloads/alard/wget-warc/wget-warc-20111017.tar.bz2
-tar xjf wget-warc-20111017.tar.bz2
+wget https://github.com/downloads/alard/wget-warc/wget-warc-20111101.tar.bz2
+tar xjf wget-warc-20111101.tar.bz2
 cd wget-1.13.4-2567-dirty/
 if ./configure && make
 then
@@ -19,7 +19,7 @@ then
   echo "wget-warc successfully built."
   echo
   ./wget-warc --help | grep -iE "gnu|warc"
-  rm -rf wget-warc-20111017.tar.bz2 wget-1.13.4-2567-dirty/
+  rm -rf wget-warc-20111101.tar.bz2 wget-1.13.4-2567-dirty/
 else
   echo
   echo "wget-warc not successfully built."
