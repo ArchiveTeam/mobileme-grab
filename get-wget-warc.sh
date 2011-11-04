7d71bf6fabd446408ecb268c7ded73e51cf173cc
Alard <alard@example.com>
2011-11-04 10:54:10 +0100

    Have wget use OpenSSL if GnuTLS is not available.

--- get-wget-warc.sh
+++ get-wget-warc.sh
@@ -3,12 +3,27 @@
 # This script downloads and compiles wget-warc.
 #
 
+# first, try to detect gnutls or openssl
+CONFIGURE_SSL_OPT=""
+if builtin type -p pkg-config &>/dev/null
+then
+  if pkg-config gnutls
+  then
+    echo "Compiling wget with GnuTLS."
+    CONFIGURE_SSL_OPT="--with-ssl=gnutls"
+  elif pkg-config openssl
+  then
+    echo "Compiling wget with OpenSSL."
+    CONFIGURE_SSL_OPT="--with-ssl=openssl"
+  fi
+fi
+
 rm -rf wget-warc-20111101.tar.bz2 wget-1.13.4-2567-dirty/
 
 wget --no-check-certificate https://github.com/downloads/alard/wget-warc/wget-warc-20111101.tar.bz2
 tar xjf wget-warc-20111101.tar.bz2
 cd wget-1.13.4-2567-dirty/
-if ./configure && make
+if ./configure $CONFIGURE_SSL_OPT && make
 then
   cp src/wget ../wget-warc
   cd ../
